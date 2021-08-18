4744b9b6381dcf007375c3936f9b749cb722ad75
Siddharth Narula <31994579+siddharthnarula1234@users.noreply.github.com>
2021-08-18 16:01:08 -0700

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+template <typename T>
+T square(T x)
+{ x=x*x;
+   return x;}
+int main()
+{cout << square<int>(3) << endl;
+  cout << square<double>(3.0121122) << endl; 
+  cout << square<float>(3.13) << endl; }
